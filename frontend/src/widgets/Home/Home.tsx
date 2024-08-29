e9b2866bf2496c2302ae021e99f095d6c9f2f10e
Oleksandr Aleksieiev <oleksandr.aleksieiev@teleport.de>
2024-08-29 16:14:09 +0200

    home contacts button added

--- frontend/src/widgets/Home/Home.tsx
+++ frontend/src/widgets/Home/Home.tsx
@@ -1,8 +1,10 @@
-import { ERouterTitle } from '@/shared/router';
+import { Button } from '@/components/shadcn/ui/button';
+import { ERouter, ERouterTitle } from '@/shared/router';
 import AppPageTitle from '@/shared/widgets/AppPageTitle';
 import AppScrollContentWrapper from '@/shared/widgets/AppScrollContentWrapper';
 import AppWrapper from '@/shared/widgets/AppWrapper';
 import { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: FC = (): ReactElement => {
   return (
@@ -10,7 +12,11 @@ const Home: FC = (): ReactElement => {
       <AppScrollContentWrapper>
         <AppPageTitle title={ERouterTitle.HOME} />
 
-        <div>Home</div>
+        <Link to={ERouter.CONTACTS}>
+          <Button variant="default" title="Contacts">
+            Contacts
+          </Button>
+        </Link>
       </AppScrollContentWrapper>
     </AppWrapper>
   );
