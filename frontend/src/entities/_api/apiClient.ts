911e9a79103693515726eeab1e20cfab51c73292
Oleksandr Aleksieiev <oleksandr.aleksieiev@teleport.de>
2024-09-10 15:55:19 +0200

    `filter with both colums added, filter type select removed

--- frontend/src/entities/_api/apiClient.ts
+++ frontend/src/entities/_api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.PROD ? '/backend' : 'http://localhost:3000',
+  baseURL: import.meta.env.PROD ? '/backend' : 'http://localhost:3000/backend',
   headers: {
     'Content-Type': 'application/json',
   },
